dc428551cc84ec898d8723785f406891d52f0a5a
ZifengChen <ken36chan@gmail.com>
2018-09-16 21:06:47 -0400

    app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Log console
-console.log("hello world");
+/*console.log("hello world");
 console.log(123);
 console.log(true);
 var greeting = 'Hello';
@@ -20,7 +20,99 @@ console.time('Hello');
 	console.log("hello world");
 console.timeEnd('Hello');
 
-/*
+
 
 	multi line comment
 */
+
+// var name ='Ken Chen';
+// console.log(name);
+
+// name = 'Steve Smith';
+// console.log(name);
+
+// //Init Var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// //Multi word vars
+
+
+// var firstName = 'John'; //Camel case
+
+
+//LET
+
+// let name ='Ken Chen';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+//CONST
+//String
+// const name = 'Ken';
+// //Number
+// const age = 45;
+// //Boolean
+// const hasKids = true;
+// //Null
+// const car = null;
+// //Undefined
+// let test;
+// //Symbol
+// const sym = Symbol();
+
+// //REFERENCE TYPES - Objects
+// //Array
+// const hobbies = ['music','movies'];
+// //Object literal
+// const address = {
+// 	city: 'West Lafayette',
+// 	state: 'IN'
+// }
+// const today = new Date();
+// console.log(today);
+// console.log(typeof today);
+
+// console.log(name);
+// //name = 'PLY';
+
+// const numbers = [1,2,3,4,5,];
+// numbers.push(6);
+
+// console.log(numbers);
+
+
+let val;
+
+val = String(5);
+val = String(4+4);
+//Bool to string
+val = String(true);
+//Date to string
+val = String(new Date());
+//Array to string
+val = String([1,2,3,4]);
+
+//toString
+val = (5).toString();
+val = (true).toString();
+
+//String to number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+
+val = parseInt('100.30');
+val = parseFloat('100.41');
+
+//Output
+console.log(val);
+console.log(typeof val);
+//console.log(val.length);
+console.log(val.toFixed());
+
